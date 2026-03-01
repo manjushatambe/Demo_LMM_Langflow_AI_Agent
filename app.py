6c6da61de30f8843e1ad9e2b674a32dc43e34f02
Manjusha Tambe <37169577+manjushatambe@users.noreply.github.com>
2026-03-01 11:10:26 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import streamlit as st
+from langflow.load import run_flow_from_json
+
+# Define your Langflow project settings
+FLOW_JSON_PATH = "LLM_Langflow_AI_Agent.json"
+
+st.title("Socratic AI Agent")
+
+if prompt := st.chat_input("Ask a question about your PDF..."):
+    # Tweaks mapping using the IDs from your uploaded file
+    tweaks = {
+      "ChatInput-UJ4St": {
+        "input_value": prompt
+      },
+      "AstraDB-gqqNc": {
+        "token": st.secrets["AstraCS:mFQPzrtclSOzDrrfkkfpWFed:b58b98a14d413ce54c826d477fe3a679f61cec53c82de06d9e16b0831f797f69"],
+        "api_endpoint": "https://d98a5850-19a2-44c4-9cd9-d9abc9f6f5bb-us-east1.apps.astra.datastax.com",
+        "collection_name": "llm_collect"
+      },
+      "GroqModel-7Tvzb": {
+        "groq_api_key": st.secrets["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"]
+      },
+      "OllamaEmbeddings-PVNW8": {
+        "base_url": "https://manjushatambe-ollama-space.hf.space" # Or your HF Space URL
+      }
+    }
+
+    with st.spinner("Processing..."):
+        result = run_flow_from_json(
+            flow=FLOW_JSON_PATH,
+            input_value=prompt,
+            tweaks=tweaks
+        )
+        # Extract and display the final message
+        st.write(result[0].outputs[0].results["message"].text)
